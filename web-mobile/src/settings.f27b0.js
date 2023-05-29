0073b664d324e9a20f5da37999e22714d7f3fdf2
2358-szu <99582190+zxy-szu@users.noreply.github.com>
2023-05-29 17:16:48 +0800

    Add files via upload

---
+++ web-mobile/src/settings.f27b0.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/Main.fire",orientation:"",jsList:[],bundleVers:{internal:"d17e2",resources:"5f4c8",main:"d0570"}};
